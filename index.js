28560e58839933b1b62a858084e0e606b6659058
Joss Manger <jossferatu@gmail.com>
2016-03-25 05:33:44 +0000

    removes comment

--- index.js
+++ index.js
@@ -22,8 +22,6 @@ var api = new ParseServer({
   }
 });
 
-//jossy woz ere
-
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
 // javascriptKey, restAPIKey, dotNetKey, clientKey
